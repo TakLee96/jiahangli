b68f759df3bb338bade6fe052c0e3b4b38b598f8
Jiahang Li <jiahang_li@outlook.com>
2014-11-03 12:37:30 -0800

    [Tak] Experiment

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use(function(req, res, next){
 
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(process.env.PORT || 5000, function() {
+var server = app.listen(5000, function() {
   console.log("[Server] Listening at %s", server.address().port);
-  console.log("IP Address: %s", process.env.IP);
 });
