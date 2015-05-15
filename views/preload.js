4fb553e6bd68cf02fce24f4d131f4f305c7b51a8
Jiahang Li <jiahang_li@outlook.com>
2015-05-15 10:31:31 -0700

    [Tak] fixed tiny issues

--- views/preload.js
+++ views/preload.js
@@ -1,10 +1,12 @@
-['bg2.jpg', 'me.jpg', 'Bitrun.jpg', 'Txtcoin.jpg', 'Wepay.png', 'Wisen.png'].forEach(function (src) {
-	var image = new Image();
-	image.src = '/assets/' + src;
-});
-
-setTimeout(function () {
-	document.body.classList.add('background');
-	document.getElementById('loaded').classList.remove('hide');
-	document.getElementById('loading').classList.add('hide');	
-}, 1500);
+function preload() {
+	['bg2.jpg', 'me.jpg', 'Bitrun.jpg', 'Txtcoin.jpg', 'Wepay.png', 'Wisen.png'].forEach(function (src) {
+		var image = new Image();
+		image.src = '/assets/' + src;
+	});
+	
+	setTimeout(function () {
+		document.body.classList.add('background');
+		document.getElementById('loaded').classList.remove('hide');
+		document.getElementById('loading').classList.add('hide');	
+	}, 1500);
+}
